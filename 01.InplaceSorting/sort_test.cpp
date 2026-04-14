99939d3a2135d3bd8dc03e6c80c9b97c16efb911
coolb111 <nikolaydikov856@gmail.com>
2026-04-14 21:27:21 +0300

    Добавил решение ДЗ

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,58 @@
 #include <gtest/gtest.h>
+#include <vector>
 
-// А вот тут collvalue.h не надо, берите просто инты
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
+TEST(BubbleSortSuite, EmptyVector) {
+    std::vector<int> v;
+    bubble_sort(v.begin(), v.end());
+    EXPECT_TRUE(v.empty());
+}
 
-TEST(FooSuite, FooTest)
-{
-    EXPECT_EQ(0, 0);
+TEST(BubbleSortSuite, SingleElement) {
+    std::vector<int> v = {42};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({42}));
+}
+
+TEST(BubbleSortSuite, BasicCase) {
+    std::vector<int> v = {5, 1, 4, 2, 8};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({1, 2, 4, 5, 8}));
+}
+
+TEST(BubbleSortSuite, EqualElements) {
+    std::vector<int> v = {7, 7, 7, 7};
+    bubble_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({7, 7, 7, 7}));
+}
+
+TEST(QuickSortSuite, EmptyVector) {
+    std::vector<int> v;
+    quick_sort(v.begin(), v.end());
+    EXPECT_TRUE(v.empty());
+}
+
+TEST(QuickSortSuite, SingleElement) {
+    std::vector<int> v = {42};
+    quick_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({42}));
+}
+
+TEST(QuickSortSuite, BasicCase) {
+    std::vector<int> v = {5, 1, 4, 2, 8};
+    quick_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({1, 2, 4, 5, 8}));
+}
+
+TEST(QuickSortSuite, ReverseOrder) {
+    std::vector<int> v = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    quick_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({1, 2, 3, 4, 5, 6, 7, 8, 9}));
+}
+
+TEST(QuickSortSuite, EqualElements) {
+    std::vector<int> v = {3, 3, 3, 3, 3};
+    quick_sort(v.begin(), v.end());
+    EXPECT_EQ(v, std::vector<int>({3, 3, 3, 3, 3}));
 }
--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -3,19 +3,52 @@
 
 // Header-only
 
-// Для swap
+#include <algorithm>
+#include <iterator>
+#include <utility>
 #include "collvalue.h"
 
-// ----------------------------- Пузырёк (например) --------------------------------------
+// ----------------------------- Пузырёк --------------------------------------
 template <typename Iterator>
-void bubble_sort(Iterator begin, Iterator end)
-{
+void bubble_sort(Iterator begin, Iterator end) {
+    if (begin == end) {
+        return;
+    }
+
+    for (Iterator last = end; last != begin; --last) {
+        bool swapped = false;
+        for (Iterator it = begin; std::next(it) != last; ++it) {
+            Iterator next = std::next(it);
+            if (*next < *it) {
+                std::iter_swap(it, next);
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            return;
+        }
+    }
 }
 
-// --------------------------- QuickSort (например) ---------------------------------------
+// --------------------------- QuickSort ---------------------------------------
 template <typename Iterator>
-void quick_sort(Iterator begin, Iterator end)
-{
+void quick_sort(Iterator begin, Iterator end) {
+    if (begin == end || std::next(begin) == end) {
+        return;
+    }
+
+    auto pivot = *std::next(begin, std::distance(begin, end) / 2);
+
+    Iterator middle1 = std::partition(begin, end, [&](const auto& value) {
+        return value < pivot;
+    });
+
+    Iterator middle2 = std::partition(middle1, end, [&](const auto& value) {
+        return !(pivot < value);
+    });
+
+    quick_sort(begin, middle1);
+    quick_sort(middle2, end);
 }
 
 #endif // SORTING_H
